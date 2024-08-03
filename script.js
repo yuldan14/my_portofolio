24332a537d7c2ce80b6b1b3c44dd50f70704b329
yuldan14 <72258620+yuldan14@users.noreply.github.com>
2024-08-03 19:42:12 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -65,3 +65,31 @@ document.addEventListener('mousemove', (event) => {
 
 
 
+
+// Burger menu
+
+
+const burger = document.getElementById('burger');
+const navLinks = document.getElementById('nav-links');
+
+// Toggle menu function
+const toggleMenu = () => {
+    navLinks.classList.toggle('active');
+    burger.classList.toggle('toggle');
+};
+
+// Burger menu click event
+burger.addEventListener('click', () => {
+    toggleMenu();
+});
+
+// Close menu when clicking outside
+document.addEventListener('click', (event) => {
+    const target = event.target;
+    const isNavMenu = target.closest('.nav-menu');
+
+    if (!isNavMenu) {
+        navLinks.classList.remove('active');
+        burger.classList.remove('toggle');
+    }
+});
